58fe3f4cd6f326f6a73bec17147ec6a9c81f4376
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-10-02 17:00:16 +0530

    drop down menu adjusted and icons added

--- home.php
+++ home.php
@@ -13,30 +13,44 @@
 </head>
 <body>
 	<div class="container">
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
- 	<a class="navbar-brand" href="home.php"><i class="fa fa-drupal fa-lg"></i></a>
-	<ul class="navbar-nav ml-auto">
-		<li class="nav-item">
-		  <div class="dropdown">
-		    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
-		    <i class="fa fa-bars fa-lg"></i>
-		    </button>
-		    <div class="dropdown-menu">
-		    	<a class="dropdown-item" href="#">Link 1</a>
-		    	<a class="dropdown-item" href="#">Link 2</a>
-		    	<a class="dropdown-item" href="#">Link 3</a>
-		    </div>
-		  </div>
-		</li>
-	</ul>
+		<nav class="navbar navbar-expand-sm bg-success navbar-dark">
+		 	<a class="navbar-brand" href="home.php"><i class="fa fa-drupal fa-lg"></i></a>
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+				  <div class="dropdown">
+				    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+				    <i class="fa fa-bars fa-lg"></i>
+				    </button>
+				    <div class="dropdown-menu">
+				    	<a class="dropdown-item" href="#">
+				    		<i class="fa m-2 fa-user-circle-o fa-lg"></i>   
+				    		Post Ad
+				    	</a>
+				    	<a class="dropdown-item" href="#">
+				    		<i class="fa m-2 fa-adn fa-lg"></i>   
+				    		My Ads
+				    	</a>
+				    	<a class="dropdown-item" href="#">
+				    		<i class="fa m-2 fa-file-text fa-lg"></i>   
+				    	My Proposal
+				    	</a>
+				    	<a class="dropdown-item" href="#">
+				    		<i class="fa m-2 fa-line-chart fa-lg"></i>   
+				    		App Usage
+				    	</a>
+				    	<a class="dropdown-item" href="#">
+				    		<i class="fa m-2 fa-user-circle-o fa-lg"></i>   
+				    		Account
+				    	</a>
+				    </div>
+				  </div>
+				</li>
+			</ul>
+		</nav>
+	</div><br>
+	<div class="container">
+	  <h3>Navbar With Dropdown</h3>
+	  <p>This example adds a dropdown menu in the navbar.</p>
 	</div>
-</nav>
-<br>
-<div class="container">
-  <h3>Navbar With Dropdown</h3>
-  <p>This example adds a dropdown menu in the navbar.</p>
-</div>
-
 </body>
-
 </html>

3225fa2e1e4be692e497066df6a362095b26e686
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-10-02 16:15:53 +0530

    created new page home.php

---
+++ home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+ 	<a class="navbar-brand" href="#">Title</a>
+	<ul class="navbar-nav ml-auto">
+		<li class="nav-item">
+		  <div class="dropdown">
+		    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+		    this
+		    </button>
+		    <div class="dropdown-menu">
+		    	<a class="dropdown-item" href="#">Link 1</a>
+		    	<a class="dropdown-item" href="#">Link 2</a>
+		    	<a class="dropdown-item" href="#">Link 3</a>
+		    </div>
+		  </div>
+		</li>
+	</ul>
+</nav>
+<br>
+<div class="container">
+  <h3>Navbar With Dropdown</h3>
+  <p>This example adds a dropdown menu in the navbar.</p>
+</div>
+
+</body>
+
+</html>

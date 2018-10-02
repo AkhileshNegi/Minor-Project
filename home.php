8ed84583e17df905045fe5f2043acf9e7940580a
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-10-02 16:43:07 +0530

    added fontawesome icons

--- home.php
+++ home.php
@@ -6,18 +6,20 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
 </head>
 <body>
+	<div class="container">
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
- 	<a class="navbar-brand" href="#">Title</a>
+ 	<a class="navbar-brand" href="home.php"><i class="fa fa-drupal fa-lg"></i></a>
 	<ul class="navbar-nav ml-auto">
 		<li class="nav-item">
 		  <div class="dropdown">
 		    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
-		    this
+		    <i class="fa fa-bars fa-lg"></i>
 		    </button>
 		    <div class="dropdown-menu">
 		    	<a class="dropdown-item" href="#">Link 1</a>
@@ -27,6 +29,7 @@
 		  </div>
 		</li>
 	</ul>
+	</div>
 </nav>
 <br>
 <div class="container">

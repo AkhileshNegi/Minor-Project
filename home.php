eb718f04218616f83dc2a521c8cf0b5d9c8104c4
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-10-06 18:16:04 +0530

    added a modal to respond button

--- home.php
+++ home.php
@@ -59,9 +59,9 @@
 				<button type="button" class="m-1 btn btn-outline-info">
 					<i class="fa fa-inr fa-lg"></i>  20
 				</button>
-				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
-				  Open modal
-				</button>
+				  <button type="button" class="m-1 btn btn-outline-warning" data-toggle="modal" data-target="#myModal">
+				    Respond
+				  </button>
 				<button type="button" class="m-1 btn btn-outline-success">4:00 pm</button>
 				</div>
 			</div>
@@ -89,5 +89,25 @@
 			</div>
 		</div>
 	</div>
+	  <div class="modal" id="myModal">
+	    <div class="modal-dialog">
+	      <div class="modal-content">
+	        <div class="modal-header">
+	          <h4 class="modal-title">Modal Heading</h4>
+	          <button type="button" class="close" data-dismiss="modal">&times;</button>
+	        </div>
+	        <div class="modal-body">
+	          Modal body..
+	        </div>
+	        <div class="modal-footer">
+	          <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+	        </div>	        
+	      </div>
+	    </div>
+	  </div>
+	  
+	</div>
+
+</div>
 </body>
 </html>

027e3b8a503dc85af893df39bad2eb45554d00f8
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-10-10 00:25:20 +0530

    added animation to the pagesin shape of a box

---
+++ journey.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Home</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="container">
+		<nav class="navbar navbar-expand-sm bg-success navbar-dark">
+		 	<a class="navbar-brand" href="home.php"><i class="fa fa-drupal fa-lg"></i></a>
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+				  <div class="dropdown">
+				    <button type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown">
+				    <i class="fa fa-bars fa-lg"></i>
+				    </button>
+				    <div class="dropdown-menu">
+				    	<a class="dropdown-item" href="post_ad.php">
+				    		<i class="fa m-2 fa-pencil fa-lg"></i>   
+				    		Post Ad
+				    	</a>
+				    	<a class="dropdown-item" href="my_ads.php">
+				    		<i class="fa m-2 fa-adn fa-lg"></i>   
+				    		My Ads
+				    	</a>
+				    	<a class="dropdown-item" href="my_proposals.php">
+				    		<i class="fa m-2 fa-file-text fa-lg"></i>   
+				    	My Proposal
+				    	</a>
+				    	<a class="dropdown-item" href="journey.php">
+				    		<i class="fa m-2 fa-line-chart fa-lg"></i>   
+				    		App Usage
+				    	</a>
+				    	<a class="dropdown-item" href="#">
+				    		<i class="fa m-2 fa-user-circle-o fa-lg"></i>   
+				    		Account
+				    	</a>
+				    	<a class="dropdown-item" href="login.html">
+				    		<i class="fa m-2 fa-sign-out fa-lg"></i>   
+				    		Logout
+				    	</a>
+				    </div>
+				  </div>
+				</li>
+			</ul>
+		</nav><br>
+		<div style="background:#0000FF;height:100px;width:100px;">
+			<div id="mover" style="border:4px solid #98bf21;height:100px;width:100px;position:absolute;">
+			</div>
+		</div>
+	</div>
+	<script src="js/main_jquery.js"></script>
+</body>
+</html>
--- js/main_jquery.js
+++ js/main_jquery.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
-    $("#post_ad").submit(function(){
-        confirm("Submitted");
-    });
+	$("#post_ad").submit(function(){
+		confirm("Submitted");
+	});
+    $("#mover").delay(1200).animate({left: '250px'});
+ 	$("#mover").delay(1200).animate({left: '500px'});
 });

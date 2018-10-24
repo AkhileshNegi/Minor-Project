79740218e0a85a839e1372e69911726bc44f2566
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-10-24 11:46:22 +0530

    added a new file delete_proposal for AJAX

--- js/main_jquery.js
+++ js/main_jquery.js
@@ -18,6 +18,19 @@ $(document).ready(function(){
   }
   $(".cancel_my_proposal").click(function(){
     var OID = $(this).data('username');
-    alert(OID);        
+    delete_proposal(OID)        
   });
+  function delete_proposal(OID){
+    jQuery.ajax({
+      type: "POST",
+      url: "delete_proposal.php",
+      data: { 
+            OID: +OID
+            },
+      cache: false,
+      success: function(data){
+        location.reload()
+      }
+    });
+  }
 });

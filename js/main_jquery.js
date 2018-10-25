29e9f95815d3711ebd8566718326411bd290baa9
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-10-26 00:06:29 +0530

    removing the posted ad on successfull payment

--- js/main_jquery.js
+++ js/main_jquery.js
@@ -22,6 +22,7 @@ $(document).ready(function(){
             },
       cache: false,
       success: function(data){
+        delete_ad(AdID)
         $("#offer_popup").html(payment_success());
         location.reload()
       }

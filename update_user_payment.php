2f3455434f76d41b562c94b872fbe056cfb3048b
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-10-26 18:54:10 +0530

    updating payment table status on paying to user

--- update_user_payment.php
+++ update_user_payment.php
@@ -1,4 +1,6 @@
 <?php
+$conn = new mysqli('localhost', 'root', '', 'alchemist');
 $Tno = $_POST['Tno'] ;
-echo $Tno;
+$user_payment = "UPDATE payments SET end_payment = 'Paid' WHERE Transaction_No = '$Tno';";
+$success_payment = mysqli_query($conn, $user_payment);
 ?>

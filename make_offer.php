601f8950ef6f840b6969b90622fb663ace4b07a9
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-11-02 00:10:25 +0530

    dont know

--- make_offer.php
+++ make_offer.php
@@ -5,4 +5,5 @@ $user_name = $_POST['user_name'];
 $conn = new mysqli('localhost', 'root', '', 'alchemist');
 $sql = "INSERT INTO proposal (price,AdID,Offered_by) VALUES('$cost','$AdID','$user_name')";
 mysqli_query($conn, $sql);
+die();
 ?>

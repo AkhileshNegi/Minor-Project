8ac8e87e2970e0bf9e566f44237bd93fbf02700e
AkhileshNegi <akhileshnegi.an3@gmail.com>
2018-11-02 13:13:35 +0530

    Fonts adjusted for navbar

--- index.php
+++ index.php
@@ -61,12 +61,14 @@ if (!empty($_SESSION["name"])) {
 	?>
 	<nav class="navbar navbar-expand-sm bg-success navbar-dark p-2">
 		<div class="container">
-			<a class="navbar-brand" href="index.php"><i class="fa fa-drupal fa-lg">
-				<span class="Logo">HillCart</span>
-			</i></a>
+			<a class="navbar-brand" href="index.php">
+				<i class="fa fa-drupal fa-lg">
+					<span class="Logo">HillCart</span>
+				</i>
+			</a>
 			<ul class="navbar-nav mx-auto">
 				<li class="nav-item">
-					<a class="dropdown-item " href="post_ad.php?name=value1" method="GET">
+					<a class="Logo dropdown-item " href="post_ad.php?name=value1" method="GET">
 						<i class="fa m-2 fa-pencil fa-lg"></i>   
 						Post Ad
 					</a>
